798b5f72fc9342d8bc055d59939ff3d370490393
nelsonchengg <nelsoncheng103@gmail.com>
2023-03-17 17:00:14 -0400

    Doing the same thing with promises

---
+++ index2.js
@@ -0,0 +1,18 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    const printPassTimes = function(passTimes) {
+      for (const pass of passTimes) {
+        const datetime = new Date(0);
+        datetime.setUTCSeconds(pass.risetime);
+        const duration = pass.duration;
+        console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+      }
+    };
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+  
